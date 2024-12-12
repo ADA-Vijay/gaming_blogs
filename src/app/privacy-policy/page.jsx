c15e45be0e67ba49a98c5cc62b405a8b42722704
Vijay saroj <vijaysaroj4527@gmail.com>
2024-12-12 15:42:22 +0530

    new changes

--- src/app/privacy-policy/page.jsx
+++ src/app/privacy-policy/page.jsx
@@ -4,7 +4,7 @@ export async function generateMetadata() {
   return {
     title: "Privacy and Policy",
     description:
-    "Discover guides and news on esports, gaming, entertainment, and tech at GameTechAnime. We provide timely coverage to keep you informed.",
+      "Discover guides and news on esports, gaming, entertainment, and tech at GameTechAnime. We provide timely coverage to keep you informed.",
     openGraph: {
       images: [
         {
@@ -23,57 +23,77 @@ const page = () => {
       <div className={styles.container}>
         <h1 className={styles["header-texts"]}>Privacy and Policy </h1>
         <span>
-          At GameTechAnime, we value your privacy and want to be clear about how we handle your
-          information. This policy explains what information we collect and why we collect it
+          At GameTechAnime, we value your privacy and want to be clear about how
+          we handle your information. This policy explains what information we
+          collect and why we collect it
         </span>
         <div className={styles.rules}>
-          <h2 className={styles["header-texts"]}>Information Collection and Use</h2>
+          <h2 className={styles["header-texts"]}>
+            Information Collection and Use
+          </h2>
           <span className={styles.lists}>
-            When you visit GameTechAnime, we automatically collect basic information through our web
-             servers and Google Analytics. This includes your IP address, browser type, operating system,
-              and which pages you visit. We use this information to understand how visitors use our site and 
-              to improve our content and user experience.
+            When you visit GameTechAnime, we automatically collect basic
+            information through our web servers and Google Analytics. This
+            includes your IP address, browser type, operating system, and which
+            pages you visit. We use this information to understand how visitors
+            use our site and to improve our content and user experience.
           </span>
           <h2 className={styles["header-texts"]}>Cookies</h2>
           <span className={styles.lists}>
-          Our site uses cookies to work properly and to help us understand site usage through Google 
-          Analytics. You can control cookies through your browser settings, though this might affect
-           how our site works for you.
+            Our site uses cookies to work properly and to help us understand
+            site usage through Google Analytics. You can control cookies through
+            your browser settings, though this might affect how our site works
+            for you.
           </span>
         </div>
         <div className={styles.rules}>
           <h2 className={styles["header-texts"]}>Google Analytics </h2>
           <span className={styles.lists}>
-          We use Google Analytics to understand our website traffic. Google Analytics has its own privacy
-           policy and collects data according to its terms. You can opt out of Google Analytics tracking by 
-           installing their opt-out browser add-on
+            We use Google Analytics to understand our website traffic. Google
+            Analytics has its own privacy policy and collects data according to
+            its terms. You can opt out of Google Analytics tracking by
+            installing their opt-out browser add-on
           </span>
         </div>
         <div className={styles.rules}>
           <h2 className={styles["header-texts"]}>Data Security</h2>
           <span className={styles.lists}>
-          We take reasonable steps to protect your information through secure servers and regular
-           security monitoring. Since we're a global website, your data may be processed on servers in
-            different countries.
+            We take reasonable steps to protect your information through secure
+            servers and regular security monitoring. Since we're a global
+            website, your data may be processed on servers in different
+            countries.
           </span>
         </div>
 
         <div className={styles.rules}>
           <h2 className={styles["header-texts"]}>Your Privacy Rights</h2>
           <span className={styles.lists}>
-          You can request information about what data we have or ask us to delete your technical data
-         where possible. We don't collect personal information unless you specifically provide it to us.
+            You can request information about what data we have or ask us to
+            delete your technical data where possible. We don't collect personal
+            information unless you specifically provide it to us.
           </span>
         </div>
         <div className={styles.rules}>
-          
           <h2 className={styles["header-texts"]}>Changes and Contact</h2>
           <span className={styles.lists}>
-          We may update this policy occasionally. For any privacy questions, email us at 
-          gametechanime@gmail.com with the subject line [PRIVACY INQUIRY].
+            We may update this policy occasionally. For any privacy questions,
+            email us at gametechanime@gmail.com with the subject line [PRIVACY
+            INQUIRY].
           </span>
         </div>
-         
+        <div div className={styles.rules}>
+          <p style={{ marginBottom: "4rem" ,fontSize:"18px"}}>
+            All or partial advertising on this Website or App is managed by
+            Playwire LLC. If Playwire publisher advertising services are used,
+            Playwire LLC may collect and use certain aggregated and anonymized
+            data for advertising purposes. To learn more about the types of data
+            collected, how data is used and your choices as a user, please visit{" "}
+            <a href="https://www.playwire.com/privacy-policy">
+              https://www.playwire.com/privacy-policy
+            </a>
+            .
+          </p>
+        </div>
       </div>
     </>
   );
